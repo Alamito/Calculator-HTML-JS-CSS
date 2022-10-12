0c029cb4329b7cf32f27278365e4eb44f8880480
Alamito <alamirbobroski12@gmail.com>
2022-10-12 17:07:09 -0300

    add print numbers in the input

--- script.js
+++ script.js
@@ -18,87 +18,110 @@ buttonSum = document.querySelector('.sum');
 buttonSubtraction = document.querySelector('.subtraction');
 buttonProduct = document.querySelector('.product');
 buttonDivision = document.querySelector('.division');
-buttonResult = document.querySelector('.result');
+buttonResult = document.querySelector('.buttonResult');
+buttonClear = document.querySelector('.buttonClear');
 
 buttonOne.addEventListener('click', () => {
     //firstNumber += '1';
     //console.log(firstNumber);
     if (operator.length === 0) {
         firstNumber = storeNumber(firstNumber, '1');
+        printResult(firstNumber);
     } else {
         secondNumber = storeNumber(secondNumber, '1');
+        printResult(secondNumber);
     }
 })
 buttonTwo.addEventListener('click', () => {
     if (operator.length === 0) {
         firstNumber = storeNumber(firstNumber, '2');
+        printResult(firstNumber);
     } else {
         secondNumber = storeNumber(secondNumber, '2');
+        printResult(secondNumber);
     }
 })
 buttonThree.addEventListener('click', () => {
     if (operator.length === 0) {
         firstNumber = storeNumber(firstNumber, '3');
+        printResult(firstNumber);
     } else {
         secondNumber = storeNumber(secondNumber, '3');
+        printResult(secondNumber);
     }
 })
 buttonFour.addEventListener('click', () => {
     if (operator.length === 0) {
         firstNumber = storeNumber(firstNumber, '4');
+        printResult(firstNumber);
     } else {
         secondNumber = storeNumber(secondNumber, '4');
+        printResult(secondNumber);
     }
 })
 buttonFive.addEventListener('click', () => {
     if (operator.length === 0) {
         firstNumber = storeNumber(firstNumber, '5');
+        printResult(firstNumber);
     } else {
         secondNumber = storeNumber(secondNumber, '5');
+        printResult(secondNumber);
     }
 })
 buttonSix.addEventListener('click', () => {
     if (operator.length === 0) {
         firstNumber = storeNumber(firstNumber, '6');
+        printResult(firstNumber);
     } else {
         secondNumber = storeNumber(secondNumber, '6');
+        printResult(secondNumber);
     }
 })
 buttonSeven.addEventListener('click', () => {
     if (operator.length === 0) {
         firstNumber = storeNumber(firstNumber, '7');
+        printResult(firstNumber);
     } else {
         secondNumber = storeNumber(secondNumber, '7');
+        printResult(secondNumber);
     }
 })
 buttonEight.addEventListener('click', () => {
     if (operator.length === 0) {
         firstNumber = storeNumber(firstNumber, '8');
+        printResult(firstNumber);
     } else {
         secondNumber = storeNumber(secondNumber, '8');
+        printResult(secondNumber);
     }
 })
 buttonNine.addEventListener('click', () => {
     if (operator.length === 0) {
         firstNumber = storeNumber(firstNumber, '9');
+        printResult(firstNumber);
     } else {
         secondNumber = storeNumber(secondNumber, '9');
+        printResult(secondNumber);
     }
 })
 buttonSum.addEventListener('click', () => {
     operator = '+'
+    printResult('');
     //console.log(firstNumber)
 })
 buttonSubtraction.addEventListener('click', () => {
     operator = '-'
+    printResult('');
     //console.log(firstNumber)
 })
 buttonProduct.addEventListener('click', () => {
     operator = '*'
+    printResult('');
     //console.log(firstNumber)
 })
 buttonDivision.addEventListener('click', () => {
     operator = '/'
+    printResult('');
     //console.log(firstNumber)
 })
 buttonResult.addEventListener('click', () => {
@@ -107,7 +130,15 @@ buttonResult.addEventListener('click', () => {
 
     console.log(calcResult(firstNumber, secondNumber));
     firstNumber = calcResult(firstNumber, secondNumber);
+    printResult(firstNumber);
 
+    operator = '';
+    secondNumber = '';
+})
+buttonClear.addEventListener('click', () => {
+    printResult('');
+
+    firstNumber = '';
     operator = '';
     secondNumber = '';
 })
@@ -134,6 +165,10 @@ function calcResult(firstNumber, secondNumber) {
             break;
     }   
     return firstNumber;
+}
+
+function printResult(number) {
+    document.querySelector('.printNumber').value = number;
 }
 
 
